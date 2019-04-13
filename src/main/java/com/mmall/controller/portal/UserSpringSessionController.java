af2730b1f1893b6f8f722c2b593c364cf16f752e
yxzhang6 <yxzhang6@163.com>
2019-04-13 14:16:42 +0800

    Spring MVC Global Exception HandlerExceptionResolver

--- src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
+++ src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
@@ -41,6 +41,11 @@ public class UserSpringSessionController {
     @RequestMapping(value = "login.do", method = RequestMethod.GET)
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session,HttpServletResponse httpServletResponse) {
+
+//        测试全局异常
+        int i = 0;
+        int j = 666/i;
+
         ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
             session.setAttribute(Const.CURRENT_USER, response.getData());
