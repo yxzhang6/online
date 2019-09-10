8991f589a1a1dfa0b8e8bd2e6dbeb79b5f95d554
yxzhang6 <yxzhang6@163.com>
2019-09-10 23:34:17 +0800

    支付对接

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -1,5 +1,9 @@
 package com.mmall.controller.portal;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.demo.trade.config.Configs;
+import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by zhang on 2019/6/26.
@@ -117,50 +123,52 @@ public class OrderController {
         return iOrderService.pay(orderNo,user.getId(),path);
     }
 
-//
-//    @RequestMapping("alipay_callback.do")
-//    @ResponseBody
-//    public Object alipayCallback(HttpServletRequest request){
-//        Map<String,String> params = Maps.newHashMap();
-//
-//        Map requestParams = request.getParameterMap();
-//        for(Iterator iter = requestParams.keySet().iterator();iter.hasNext();){
-//            String name = (String)iter.next();
-//            String[] values = (String[]) requestParams.get(name);
-//            String valueStr = "";
-//            for(int i = 0 ; i <values.length;i++){
-//
-//                valueStr = (i == values.length -1)?valueStr + values[i]:valueStr + values[i]+",";
-//            }
-//            params.put(name,valueStr);
-//        }
-//        logger.info("支付宝回调,sign:{},trade_status:{},参数:{}",params.get("sign"),params.get("trade_status"),params.toString());
-//
-//        //非常重要,验证回调的正确性,是不是支付宝发的.并且呢还要避免重复通知.
-//
-//        params.remove("sign_type");
-//        try {
-//            boolean alipayRSACheckedV2 = AlipaySignature.rsaCheckV2(params, Configs.getAlipayPublicKey(),"utf-8",Configs.getSignType());
-//
-//            if(!alipayRSACheckedV2){
-//                return ServerResponse.createByErrorMessage("非法请求,验证不通过,再恶意请求我就报警找网警了");
-//            }
-//        } catch (AlipayApiException e) {
-//            logger.error("支付宝验证回调异常",e);
-//        }
-//
-//        //todo 验证各种数据
-//
-//
-//        //
-//        ServerResponse serverResponse = iOrderService.aliCallback(params);
-//        if(serverResponse.isSuccess()){
-//            return Const.AlipayCallback.RESPONSE_SUCCESS;
-//        }
-//        return Const.AlipayCallback.RESPONSE_FAILED;
-//    }
+
+    //支付宝回调函数实现
+    @RequestMapping("alipay_callback.do")
+    @ResponseBody
+    public Object alipayCallback(HttpServletRequest request){
+        Map<String,String> params = Maps.newHashMap();
+
+        Map requestParams = request.getParameterMap();
+        for(Iterator iter = requestParams.keySet().iterator(); iter.hasNext();){
+            String name = (String)iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for(int i = 0 ; i <values.length;i++){
+
+                valueStr = (i == values.length -1)?valueStr + values[i]:valueStr + values[i]+",";
+            }
+            params.put(name,valueStr);
+        }
+        logger.info("支付宝回调,sign:{},trade_status:{},参数:{}",params.get("sign"),params.get("trade_status"),params.toString());
+
+        //非常重要,验证回调的正确性,是不是支付宝发的.并且呢还要避免重复通知.
+
+        params.remove("sign_type");
+        try {
+            boolean alipayRSACheckedV2 = AlipaySignature.rsaCheckV2(params, Configs.getAlipayPublicKey(),"utf-8",Configs.getSignType());
+
+            if(!alipayRSACheckedV2){
+                return ServerResponse.createByErrorMessage("非法请求,验证不通过,再恶意请求我就报警找网警了");
+            }
+        } catch (AlipayApiException e) {
+            logger.error("支付宝验证回调异常",e);
+        }
+
+        //todo 验证各种数据
 
 
+        //
+        ServerResponse serverResponse = iOrderService.aliCallback(params);
+        if(serverResponse.isSuccess()){
+            return Const.AlipayCallback.RESPONSE_SUCCESS;
+        }
+        return Const.AlipayCallback.RESPONSE_FAILED;
+    }
+
+
+    //轮询查询订单的支付状态
     @RequestMapping("query_order_pay_status.do")
     @ResponseBody
     public ServerResponse<Boolean> queryOrderPayStatus(HttpSession session, Long orderNo){
